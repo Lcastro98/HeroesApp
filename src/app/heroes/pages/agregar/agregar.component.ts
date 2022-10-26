b063462ad7d01fa7218f6a765526260ffa152ec6
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-26 17:06:07 -0500

    Editar heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-agregar',
@@ -29,9 +31,17 @@ export class AgregarComponent implements OnInit {
     characters: '',
     alt_img: ''
   }
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService,
+              private activateRoute: ActivatedRoute,
+              private route: Router) { }
 
   ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroePorId(id))
+      )
+      .subscribe(heroe => this.heroe = heroe
+      );
   }
 
   guardar() {
@@ -39,11 +49,16 @@ export class AgregarComponent implements OnInit {
       return;
     }
 
-    this.heroesService.agregarHeroe(this.heroe)
-      .subscribe(resp => {
-        console.log('Respuesta', resp);
-        
+    if(this.heroe.id) {
+      this.heroesService.actualizarHeroe(this.heroe)
+        .subscribe(heroe => console.log('Actualizando', heroe)
+        )
+    } else {
+      this.heroesService.agregarHeroe(this.heroe)
+        .subscribe(heroe => {
+          this.route.navigate(['/heroes/editar', heroe.id]);
       })
+    }
   }
 
 }
