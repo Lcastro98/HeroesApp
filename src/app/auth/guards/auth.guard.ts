a4328a920ad6bb26769e0b071d4b9e76a0f6847e
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-28 07:50:35 -0500

    CanActivate

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, TitleStrategy, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanLoad, CanActivate {
 
   constructor(private authService: AuthService){}
 
-  /*canActivate(
+  canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }*/
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if(this.authService.auth.id){
+        return true;
+      }
+
+      console.log('Bloqueado por el AuthGuard - CanActivate');
+      return false;
+  }
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
@@ -23,7 +29,7 @@ export class AuthGuard implements CanLoad {
         return true;
       }
 
-      console.log('Bloqueado por el AuthGuard');
+      console.log('Bloqueado por el AuthGuard - CanLoad');
       return false;
   }
 }
